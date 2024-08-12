5639ca3b7862d06d5144afd307e27da2aaba810d
SimonOkello <simonokello93@gmail.com>
2024-08-12 10:05:30 +0300

    create prompt

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,12 +1,38 @@
-'use client'
+'use client';
 
-import { useState,useEffect } from "react"
-import PromptCard from "./PromptCard"
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+	return <div className="mt-16 prompt_layout"></div>;
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+	const [searchText, setSearchText] = useState('');
 
-export default Feed
+	const handleSearchChange = (e) => {};
+
+	useEffect(() => {
+		const fetchPrompts = async () => {
+			const response = await fetch('/api/prompt');
+			const data = response.json();
+      
+		};
+	}, []);
+	return (
+		<section className="feed">
+			<form className="relative flex-center w-full">
+				<input
+					type="text"
+					placeholder="Search tag or username"
+					value={searchText}
+					onChange={handleSearchChange}
+					className="search_input peer"
+				/>
+			</form>
+			<PromptCardList data={[]} handleTagClick={() => {}} />
+		</section>
+	);
+};
+
+export default Feed;
